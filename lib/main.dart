ef0d58a223ea54e442381277b9eaad34efd6dfd6
tatarbai <pinkinlark@mail.ru>
2023-01-05 13:12:15 +0300

    [task #3] Use sorter lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+// Package imports:
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
